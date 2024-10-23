6ab6cd839b6937b226e1afc9097eaacbd387a1e1
shun <1ntegrale9uation@gmail.com>
2024-10-23 23:33:06 +0900

    update

--- extensions/auto_reaction.py
+++ extensions/auto_reaction.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 from utils import extract_emojis
 from utils.database import cache_emojis
@@ -6,6 +7,7 @@ from utils.database import get_emojis
 from utils.database import upsert_emojis
 from utils.database import delete_emojis
 from daug.utils.dpyexcept import excepter
+from daug.utils.dpylog import dpylogger
 from daug.constants import COLOUR_EMBED_GRAY
 
 
@@ -47,6 +49,44 @@ class AutoReactionCog(commands.Cog):
                 await message.add_reaction(emoji)
             except discord.errors.HTTPException:
                 pass
+
+
+    @app_commands.command(name='リアクション設定確認', description='現在設定されているリアクションを確認します')
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.guild_only()
+    @excepter
+    @dpylogger
+    async def _reaction_for_channel_app_command(self, interaction: discord.Interaction):
+        emojis = get_emojis(interaction.channel.id)
+        await interaction.response.send_message(
+            embed=discord.Embed(title='設定した絵文字', description=' '.join(emojis), colour=COLOUR_EMBED_GRAY),
+        )
+
+
+    @app_commands.command(name='リアクション補完', description='チャンネル内のすべてのメッセージにリアクションを付けます')
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.guild_only()
+    @excepter
+    @dpylogger
+    async def _reaction_for_channel_app_command(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
+        emojis = get_emojis(interaction.channel.id)
+        await interaction.followup.send(
+            'チャンネル内のすべてのメッセージにリアクションを付けます',
+            embed=discord.Embed(title='設定中の絵文字', description=' '.join(emojis), colour=COLOUR_EMBED_GRAY),
+            ephemeral=True,
+        )
+        async for message in interaction.channel.history(limit=None):
+            for emoji in emojis:
+                try:
+                    await message.add_reaction(emoji)
+                except discord.errors.HTTPException:
+                    pass
+        await interaction.followup.send(
+            'チャンネル内のすべてのメッセージにリアクションを付けました',
+            embed=discord.Embed(title='設定中の絵文字', description=' '.join(emojis), colour=COLOUR_EMBED_GRAY),
+            ephemeral=True,
+        )
 
 
 async def setup(bot: commands.Bot) -> None:
